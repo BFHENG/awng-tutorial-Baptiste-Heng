c8db06c133f728316954431d34cc8a4c0f285cb2
BFHENG <baptiste.heng.s@gmail.com>
2022-04-07 11:01:44 +0200

    Pointage confiv vers URL racine

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
 from . import views
+from django.contrib import admin
+from django.urls import include, path
 
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
 urlpatterns = [
     path('', views.index, name='index'),
 ]
