f5ba1ccd47847fd444408d7c104b857835010c8a
BFHENG <baptiste.heng.s@gmail.com>
2022-04-12 10:44:25 +0200

    Views fonctionnel pour vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
-#from django.http import HttpResponse
-#from django.template import loader
+
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.urls import reverse
 
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
